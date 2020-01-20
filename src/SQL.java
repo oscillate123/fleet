c3118ee902b4194e87641da1df9bee1840e79fe1
Oscar J Mac <neonode2014@gmail.com>
2020-01-20 21:35:14 +0100

    updating sql method
    Signed-off-by: Oscar J Mac <neonode2014@gmail.com>

--- src/SQL.java
+++ src/SQL.java
@@ -6,7 +6,7 @@ import java.sql.Statement;
 public class SQL {
 
     @SuppressWarnings("unused")
-    private final String useSSLFalse  = "?useSSL=false";
+    private static final String useSSLFalse  = "?useSSL=false";
     private static final String useSSLTrue   = "?useSSL=true";
     private static final String DATABASE     = "ships";
     private static final String URL          = "jdbc:mysql://flottan.mysql.database.azure.com:3306/" + DATABASE + useSSLTrue;
@@ -16,8 +16,10 @@ public class SQL {
 
     public static void getDatabases () {
 
-        // String sql_query = "select * from " + table + ";";
-        String sql_query = "show databases;";
+        String table = "object_state_log";
+
+        String sql_query = "select * from " + table + ";";
+        // String sql_query = "show databases;";
 
         try {
 
@@ -27,11 +29,29 @@ public class SQL {
             ResultSet myResult = myStatement.executeQuery(sql_query);
 
             while (myResult.next()) {
-                System.out.println(myResult.getString("Database"));
+
+                String objectID = myResult.getString("object_id");
+                String objectType = myResult.getString("object_type");
+                boolean isDocked = (myResult.getInt("is_docked") == 1);
+                int containerSum = myResult.getInt("container_sum");
+                int xAxis = myResult.getInt("x_axis");
+                int yAxis = myResult.getInt("y_axis");
+
+                String titles = "    ID   |     TYPE      |DOCKED |SUM |  X   |  Y";
+                System.out.println(titles);
+
+                String messagePart1 = objectID + " | " + objectType + " | "  + isDocked + " | ";
+                String messagePart2 = toString(containerSum) + " | X: " + toString(xAxis) + " | Y: " + toString(yAxis);
+                String message = messagePart1 + messagePart2;
+                System.out.println(message);
             }
 
         } catch (Exception exc) {
             exc.printStackTrace();
         }
+    }
+
+    private static String toString (int num) {
+        return Integer.toString(num);
     }
 }
