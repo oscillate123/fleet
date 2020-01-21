df08134bbfb042ee927e2f2f3681e299ed7846d4
Oscar J Mac <neonode2014@gmail.com>
2020-01-21 13:17:06 +0100

    xx

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,28 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        print_string("kek");
+        print_string("  -- Hello ladies and gentlemen this is your Main Method speaking. We are now flying --  ");
 
         String objectID = "BOAT1234";
-        ArrayList<String> kek = SQL.getOneObjectAsArrayList(objectID);
+        ArrayList<String> l = SQL.getOneObjectAsArrayList(objectID);
 
-        Ship boat = new Ship(kek.get(0), kek.get(1), stringToInt(kek.get(2)),
-                stringToInt(kek.get(3)), stringToInt(kek.get(4)), stringToInt(kek.get(5)), false);
+        Ship boat = new Ship(l.get(0), l.get(1), strToInt(l.get(2)),
+                strToInt(l.get(3)), strToInt(l.get(4)), strToInt(l.get(5)), false);
 
         if (!boat.getIsDocked()) {
             print_string("it works");
         }
 
         for (String object_id : SQL.getAllObjectIDs()) {
-            print_string(object_id);
+            print_string("\n");
+            print_string("ID:   " + object_id);
+            print_string("Type: " + SQL.getObjectType(objectID));
         }
 
     }
 
-    public static void print_string(String text) {
-        System.out.println("\n" + text);
-    }
-
-    public static void print_int(int x) {
-        System.out.println("\n" + x);
-    }
-
-    public static int stringToInt (String str) { return Integer.parseInt(str); }
+    public static void print_string(String text) { System.out.println(text); }
+    public static void print_int(int x) { System.out.println(x); }
+    public static int strToInt(String str) { return Integer.parseInt(str); }
 
 }
