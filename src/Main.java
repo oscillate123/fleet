8131d59862148cb45c419056026cbed1e4217735
Oliver <ojangholm1@hotmail.com>
2020-01-21 15:33:53 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,7 @@ public class Main {
         xDict.put("testboat1234", "25,24");
         GridMap newMap = new GridMap(25);
         newMap.drawMap(boatsID, xDict);
+        System.out.println("heej");
 
         /*
         System.out.println("\n ## IF YOU GET AN ERROR MESSAGE THAT INCLUDES AN IP ADRESS, SEND IT TO OSCAR! ## \n\n");
