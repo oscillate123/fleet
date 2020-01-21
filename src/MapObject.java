072db3fd603ef883c2d947b0186ab01135e011a8
Oscar J Mac <neonode2014@gmail.com>
2020-01-21 09:01:30 +0100

    adding one thing in ships

--- src/MapObject.java
+++ src/MapObject.java
@@ -1,20 +1,18 @@
-public class MapObjects {
+public class MapObject {
 
     private String objectID;
     private String objectType;
     private int xAxis;
     private int yAxis;
 
-    MapObjects(String objectID, String objectType, int xAxis, int yAxis) {
+    MapObject(String objectID, String objectType, int xAxis, int yAxis) {
         this.objectID = objectID;
         this.objectType = objectType;
         this.xAxis = xAxis;
         this.yAxis = yAxis;
     }
 
-    public int getxAxis() {
-        return SQL.getObjectX(this.objectID);
-    }
+    public int getxAxis() { return SQL.getObjectX(this.objectID); }
 
     public int getyAxis() {
         return SQL.getObjectY(this.objectID);
--- src/Ship.java
+++ src/Ship.java
@@ -1,10 +1,11 @@
 
-public class Ship extends MapObjects{
+public class Ship extends MapObject {
 
-    Ship(String objectID, String objectType, int xAxis, int yAxis) {
+    int containerAmount;
+
+    Ship(String objectID, String objectType, int xAxis, int yAxis, int containerAmount) {
         super(objectID, objectType, xAxis, yAxis);
+        this.containerAmount = containerAmount;
     }
-
-
 
 }
