f7362110a2bf612e28d6889fb43d3ee9381dbac5
Oscar J Mac <neonode2014@gmail.com>
2020-01-23 22:25:06 +0100

    big update

--- src/SuppFunc.java
+++ src/SuppFunc.java
@@ -1,14 +1,24 @@
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.security.Key;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Set;
+import java.sql.SQLException;
+import java.util.*;
 
 public class SuppFunc {
 
     // TODO: SuppFunc = Support Functions
     // Here we have supportive functions, to make easy things more effective and use less code.
     // We don't want to repeat ourselves (DRY)
+
+    // testing methods
+    public static void main() throws SQLException {
+
+        SQL sql = new SQL();
+        Ship ship = new Ship("vasa", false);
+
+        ArrayList<String> kek = getQueueFromUser();
+        print_string(kek.toString());
+    }
 
     // Static class properties
     private static Scanner scan = new Scanner(System.in);
@@ -51,5 +61,64 @@ public class SuppFunc {
         System.out.println("\n\n\n");
     }
 
+    public static ArrayList<String> getQueueFromUser(SQL sqlConnection, Ship ship) {
+
+        ArrayList<String> harbors = sqlConnection.getAllObjectIDs("harbor", true);
+        ArrayList<String> deliverySchedule = new ArrayList<>();
+
+        while (deliverySchedule.size() > 0) {
+            print_string("Available destinations: ");
+            for (String item : harbors) { print_string(item); }
+            print_string("");
+
+            String harborResult = "";
+            while (isStringInArray(harbors, harborResult) & harborResult.equals("exit")) {
+                harborResult = getStringInput("Which harbor do you want to go to?");
+                if (harborResult.equals("exit")) {
+                    return new ArrayList<String>(Arrays.asList("exit"));
+                }
+            }
+
+            boolean isNotBigger = true;
+            int max = 11;
+            int containerAmount = 1000000000; // TODO: Make this part of the code more efficient
+            while (containerAmount > max & isNotBigger) {
+                try {
+                    String message1 = "\nUsage: '10' for getting 10 containers, and '-10' for dropping 10 containers.";
+                    String message3 = "\nYou can maximally drop/retrieve 10 containers at the time.";
+                    String message2 = "\nHow many containers do you want to drop or pickup at " + harborResult + " ?";
+                    containerAmount = getIntInput(message1 + message3 + message2);
+
+                    int newHarborContainerSum = sqlConnection.getObjectPostInt(harborResult, sqlConnection.qObjConSum) + containerAmount;
+
+                    isNotBigger = compareVal(0, newHarborContainerSum);
+
+                } catch (Exception e) {
+                    print_string("Incorrect input.");
+                }
+            }
+
+
+
+            deliverySchedule.add(harborResult + intToStr(containerAmount));
+        }
+
+        /*
+        deliverySchedule.add(getStringInput(""));
+
+
+        deliverySchedule.add("nw_harbor,10");
+        deliverySchedule.add("sw_harbor,-5");
+        deliverySchedule.add("se_harbor,15");
+        deliverySchedule.add("c_harbor,-10");
+        deliverySchedule.add("ne_harbor,-5");
+         */
+
+        return deliverySchedule;
+    }
+
+    public static boolean compareVal(int val1, int val2) {
+        return val1 > val2;
+    }
 
 }
